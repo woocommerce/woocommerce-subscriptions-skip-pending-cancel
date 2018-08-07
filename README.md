813e9c52f38a58874808eec4e8e726eddd83d1b5
Jess Lundie <lundie.jess@gmail.com>
2018-08-06 19:17:29 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# WooCommerce Subscriptions - Skip Pending Cancel
+
+For use with [WooCommerce Subscriptions](https://woocommerce.com/products/woocommerce-subscriptions/).
+
+Install and activate this plugin. It will skip the Pending Cancellation subscription status when a customer cancels their subscription from their Account page.
+
+## Installation
+
+To install:
+
+1. Download the latest version of the plugin [here](https://github.com/Prospress/woocommerce-subscriptions-skip-pending-cancel/archive/master.zip)
+1. Go to **Plugins > Add New > Upload** administration screen on your WordPress site
+1. Select the ZIP file you just downloaded
+1. Click **Install Now**
+1. Click **Activate**
+
+### Updates
+
+To keep the plugin up-to-date, use the [GitHub Updater](https://github.com/afragen/github-updater).
+
+## Reporting Issues
+
+If you find an problem or would like to request this plugin be extended, please [open a new Issue](https://github.com/Prospress/woocommerce-subscriptions-skip-pending-cancel/issues/new).
+
+---
+
+<p align="center">
+	<a href="https://prospress.com/">
+		<img src="https://cloud.githubusercontent.com/assets/235523/11986380/bb6a0958-a983-11e5-8e9b-b9781d37c64a.png" width="160">
+	</a>
+</p>
---
+++ woocommerce-subscriptions-skip-pending-cancel.php
@@ -0,0 +1,48 @@
+<?php
+/*
+ * Plugin Name: WooCommerce Subscriptions - Skip Pending Cancel
+ * Plugin URI: https://github.com/Prospress/woocommerce-subscriptions-skip-pending-cancel/
+ * Description: Skips Pending Cancellation status when customer cancels subscription from Account page
+ * Author: Prospress Inc.
+ * Author URI: https://prospress.com/
+ * License: GPLv3
+ * Version: 1.0.0
+ * Requires at least: 4.0
+ * Tested up to: 4.8
+ *
+ * GitHub Plugin URI: Prospress/woocommerce-subscriptions-skip-pending-cancel
+ * GitHub Branch: master
+ *
+ * Copyright 2018 Prospress, Inc.  (email : freedoms@prospress.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package		WooCommerce Subscriptions - Skip Pending Cancel
+ * @author		Prospress Inc.
+ * @since		1.0
+ */
+
+require_once( 'includes/class-pp-dependencies.php' );
+
+if ( false === PP_Dependencies::is_woocommerce_active( '3.0' ) ) {
+	PP_Dependencies::enqueue_admin_notice( 'WooCommerce Subscriptions - Skip Pending Cancel', 'WooCommerce', '3.0' );
+	return;
+}
+
+if ( false === PP_Dependencies::is_subscriptions_active( '2.1' ) ) {
+	PP_Dependencies::enqueue_admin_notice( 'WooCommerce Subscriptions - Skip Pending Cancel', 'WooCommerce Subscriptions', '2.1' );
+	return;
+}
+
+add_filter( 'woocommerce_subscription_use_pending_cancel', '__return_false' );
